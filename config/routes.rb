3fb6166edd38b90e2fa537afcb50fbc8995a04c1
Elliott Reid <elliott.3115@gmail.com>
2013-12-06 10:02:34 +0000

    basic blogging CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 RailsinstallerOsxDemo::Application.routes.draw do
   get "welcome/index"
+  root 'welcome#index'
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -54,4 +57,5 @@ RailsinstallerOsxDemo::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
